c1cb729305a6ebc9984a17c5f2889d14af43e37e
HENRIQUE <henrique_holtz@hotmail.com>
2020-12-23 23:24:52 -0300

    Created background and main component

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,10 @@
+import './Calculator.css';
+import React, { Component } from 'react';
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator"></div>
+        );
+    }
+};
